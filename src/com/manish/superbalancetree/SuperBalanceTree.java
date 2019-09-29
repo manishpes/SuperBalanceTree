5a0548b831c4f04fc6c45873b438501bcc06a5e4
Manish <apple@apples-MacBook-Air.local>
2019-09-30 00:03:09 +0530

    tree

--- src/com/manish/superbalancetree/SuperBalanceTree.java
+++ src/com/manish/superbalancetree/SuperBalanceTree.java
@@ -2,3 +2,14 @@ package com.manish.superbalancetree;
 
 public class SuperBalanceTree {
 }
+
+class Tree {
+    int value;
+    Tree left, right;
+
+    Tree(int value){
+        this.value = value;
+        left = right = null;
+    }
+    
+}
